4dba7f437c1610137770b0f198b23ec66b15d272
bshivaraj <shivarajbiradar66@gmail.com>
2022-07-09 19:26:23 +0530

    first problem

--- Datastrucre/src/arraysAndHashing/ContainsDuplicate.java
+++ Datastrucre/src/arraysAndHashing/ContainsDuplicate.java
@@ -1,9 +1,22 @@
 package arraysAndHashing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
 	
 	public static void main(String[] args) {
-		System.out.println("hi");
+	     int nums[] = {1,2,3,4};
+		System.out.println(containsDuplicate(nums));
 	}
+     public static boolean containsDuplicate(int[] nums) {
+    	Set set=new HashSet();
+    	for(int i=0;i<nums.length;i++) {
+    		if(!set.add(nums[i])) {
+    			return true;
+    		}
+    	}
+		return false;
+    }
 
 }
