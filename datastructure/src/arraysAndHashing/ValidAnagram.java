5fbf94d4fdea1f1960a937d8302a5a980579abe6
bshivaraj <shivarajbiradar66@gmail.com>
2022-07-10 00:54:25 +0530

    second solution

---
+++ datastructure/src/arraysAndHashing/ValidAnagram.java
@@ -0,0 +1,19 @@
+package arraysAndHashing;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+	
+	public static void main(String[] args) {
+		System.out.println(isAnagram("anagram","nagaram"));
+	}
+	 public static boolean isAnagram(String s, String t) {
+         char c1[]=s.toCharArray();
+         char c2[]=t.toCharArray();
+        Arrays.sort(c1);
+         Arrays.sort(c2);
+		return  Arrays.equals(c1, c2);
+        
+    }
+
+}
