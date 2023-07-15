835002eb55c50ec0128ff82cc684284a07fae58e
bshivaraj <shivarajbiradar66@gmail.com>
2023-07-15 23:00:23 +0530

    maximum sub array

---
+++ datastructure/src/leetcode/Algorithm_0_1_Knapsack.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class Algorithm_0_1_Knapsack {
+	
+	public static void main(String[] args) {
+		int values[]= {1,3,4,6};
+		int wieght[]= {20,30,10,50};
+		
+		
+	}
+
+}
---
+++ datastructure/src/leetcode/LongestSubArray.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class LongestSubArray {
+	
+	public static void main(String[] args) {
+		int arr[]={1,2,3,7,5},sum=12;
+		int res[]=getBounderies(arr,sum);
+		System.out.println(res[0]+"  "+res[1]);
+	}
+
+	private static int[] getBounderies(int[] arr, int sum) {
+		int res[]=new int[2];int diff=0;
+		for(int i=0;i<arr.length-1;i++) {
+			int temp=arr[i],t1=0,j2=0;
+			for(int j=i+1;j<arr.length;j++) {
+				 temp=temp+arr[j];
+				 if(temp==sum) {
+					 j2=j;
+					 t1=j-i;
+				 }
+			}
+			if(t1>diff) {
+				diff=t1;
+				res[0]=i+1;
+				res[1]=j2+1;
+			}
+		}
+		return res;
+	}
+
+}
