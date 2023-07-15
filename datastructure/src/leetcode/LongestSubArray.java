07976f32da5fdbcb41110a5f4d8f4016c946dfa6
bshivaraj <shivarajbiradar66@gmail.com>
2023-07-15 23:07:28 +0530

    maximum sub array

--- datastructure/src/leetcode/LongestSubArray.java
+++ datastructure/src/leetcode/LongestSubArray.java
@@ -3,13 +3,17 @@ package leetcode;
 public class LongestSubArray {
 	
 	public static void main(String[] args) {
-		int arr[]={1,2,3,4,5,6,7,8,9,10},sum=15;
+		int arr[]={1,2,3,4,5,0,0,0,6,7,8,9,10},sum=15;
 		int res[]=getBounderies(arr,sum);
 		System.out.println(res[0]+"  "+res[1]);
 	}
 
 	private static int[] getBounderies(int[] arr, int sum) {
 		int res[]=new int[2];int diff=0;
+		if(arr.length==0 || sum==0) {
+			return res;
+		}
+		
 		for(int i=0;i<arr.length-1;i++) {
 			int temp=arr[i],t1=0,j2=0;
 			for(int j=i+1;j<arr.length;j++) {
