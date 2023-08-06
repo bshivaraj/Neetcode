3333d21d84505236d2bdc71ab301bd701073eaf8
bshivaraj <shivarajbiradar66@gmail.com>
2023-08-07 00:42:48 +0530

    adding wordsubset problem solution

--- datastructure/src/leetcode/WordSubSets_916.java
+++ datastructure/src/leetcode/WordSubSets_916.java
@@ -1,11 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class WordSubSets_916 {
 	
@@ -13,57 +9,43 @@ public class WordSubSets_916 {
 		String words1[] = {"amazon","apple","facebook",
 				"google","leetcode"}; 
 		String 	words2[] = {"lo","eo"};
-		List<String> res=	getResult(words1,words2);
+		List<String> res=getResult(words1,words2);
 		System.out.println(res);
 	}
 
 	private static List<String> getResult(String[] words1, String[] words2) {
+		
+		int maxword2[]=new int[26];
 		List<String> result=new ArrayList<String>();
-		List<Map<Character,Integer>> listmap=new ArrayList<Map<Character,Integer>>();
-		Map<Character,Integer> map = null;
-		for(String s:words2) {
-			map=new TreeMap<Character,Integer>();
-			for(Character c:s.toCharArray()) {
-				if(map.containsKey(c)) {
-					map.put(c,map.get(c)+1);
-				}else {
-					map.put(c, 1);
-				}
-			}
-			listmap.add(map);
-		}
-		for(String s:words1) {
-			List<Map<Character,Integer>> maps=new ArrayList<Map<Character,Integer>>();
-			for(Map<Character,Integer> m:listmap) {
-				Map<Character, Integer> tmp = null;
-				for (Map.Entry<Character, Integer> entry : m.entrySet()) {
-					tmp=new TreeMap<Character,Integer>();
-					tmp.put(entry.getKey(), entry.getValue());
-				}
-				maps.add(tmp);
-			}
-			
-			
-			char[] arr=s.toCharArray();
-			Arrays.sort(arr);
-			for(Character c:arr) {
-				for(Map<Character,Integer> tmp:maps) {
-					if(tmp.containsKey(c)) {
-						   tmp.put(c, tmp.get(c)-1);
-						   if(tmp.get(c)==(int)0) {
-							   tmp.remove(c);
-						   }
-						   if(tmp.isEmpty()) {
-							   if(!result.contains(s)) {
-								   result.add(s);
-							   }
-							}
-					   }
-				}
-			   
+		for(String s2:words2) {
+			int freq[]=getFrequency(s2);
+			for(int j=0;j<26;j++) {
+				maxword2[j]=Integer.max(maxword2[j], freq[j]);
 			}
 		}
+		for(String s1:words1) {
+			int freq[]=getFrequency(s1);
+			boolean valid=true;
+			for(int j=0;j<26;j++) {
+				if(maxword2[j]>freq[j]) {
+					valid=false;
+					break;
+				}
+			}
+			if(valid) {
+				result.add(s1);
+			}
+		}
+		
 		return result;
+	}
+	
+	private static int[] getFrequency(String s) {
+		int arr[]=new int[26];
+		for(char c:s.toCharArray()) {
+			arr[c-'a']++;
+		}
+		return arr;
 	}
 
 }
